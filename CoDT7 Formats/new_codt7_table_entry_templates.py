a44243dba392c4cdea69afafc9393a5bb2349a23
Jake-NotTheMuss <jryan7852@gmail.com>
2021-12-28 22:12:15 -0800

    Add commands for generating new files from templates

---
+++ CoDT7 Formats/new_codt7_table_entry_templates.py
@@ -0,0 +1,84 @@
+import sublime, sublime_plugin
+
+DEBUG_ON = False
+
+def debug(*args):
+    if not DEBUG_ON:
+        return
+    print(*args)
+
+def replace_rows_with_template(view, edit, template):
+    # list of cols from template string
+    template_cols = template.split(',')
+    # number of columns in template string
+    n_template_cols = len(template_cols)
+    # position to put cursor after inserting template
+    final_cursor_pos = view.sel()[0].begin()
+    # list of new selections to add afterward
+    new_selections = []
+    for region in view.sel():
+        debug('Inserting template into selection', region)
+        # absolute position of cursor at this selection
+        cursor_pos = region.begin()
+        new_selections.append(cursor_pos)
+        # region covering the entire line
+        line_region = view.line(cursor_pos)
+        # number of characters in line before editing
+        original_line_size = line_region.size()
+        # relative position of cursor in this line
+        line_pos = cursor_pos - line_region.begin()
+        debug('Inserting template at position', line_pos, 'in line')
+        # string of characters in the line
+        str_line = view.substr(line_region)
+        debug('Line:', str_line)
+        # list of column values in the line
+        row_cols = str_line.split(',')
+        # original number of columns before editing
+        # used for debugging
+        n_cols_before = len(row_cols)
+        # number of columns before the cursor position
+        # in the line
+        n_leading_cols = str_line.count(',', 0, line_pos) + 1
+        # cursor might be past the number of expected
+        # columns
+        if n_leading_cols >= n_template_cols:
+            continue
+        # new list of columns with the template inserted
+        new_row_cols = row_cols[0:n_leading_cols] + template_cols[n_leading_cols:]
+        # string of characters in edited line
+        str_new_row = ','.join(new_row_cols)
+        # number of characters in line after editing
+        new_line_size = len(str_new_row)
+        # make the changes in the buffer
+        view.replace(edit, line_region, str_new_row)
+
+    # clear selections and add back the same selections
+    # but each only spanning a single position.
+    view.sel().clear()
+    for new_sel in new_selections:
+        view.sel().add(sublime.Region(new_sel))
+
+class NewSoundAliasEntryCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        template = 'NAME,,loaded,filespec.wav,,,TEMPLATE_NAME,,,,,BUS_FX,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'
+        replace_rows_with_template(self.view, edit, template)
+
+class NewSoundReverbEntryCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        template = 'NAME,,,,,,,,,,,,,,,,,,,,,,,,\n'
+        replace_rows_with_template(self.view, edit, template)
+
+class NewSoundT5AliasEntryCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        template = 'NAME,,TEMPLATE_NAME,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'
+        replace_rows_with_template(self.view, edit, template)
+
+class NewSoundT5ReverbEntryCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        template = ',,,,,,,,,,,,'
+        replace_rows_with_template(self.view, edit, template)
+
+class NewSoundAmbientEntryCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        template = ',,,,,,,,,,,,,,,,,'
+        replace_rows_with_template(self.view, edit, template)
---
+++ CoDT7 Formats/new_codt7_templates.py
@@ -0,0 +1,117 @@
+import sublime, sublime_plugin
+
+def insert_template(view, prepend, append = ''):
+    cursor_pos = len(prepend)
+    view.run_command('append', {'characters': prepend + append})
+    view.sel().clear()
+    view.sel().add(sublime.Region(cursor_pos))
+
+class NewCodt7LocalizedStringsCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        v = self.window.new_file()
+
+        v.assign_syntax('Packages/CoDT7 Formats/LocalizedStrings.sublime-syntax')
+
+        prepend = 'VERSION              "1"\n' \
+                  'CONFIG               "C:\\projects\\cod\\t7\\bin\\StringEd.cfg"\n' \
+                  'FILENOTES            ""\n\n'
+        append  = '\n\nENDMARKER'
+
+        insert_template(v, prepend, append)
+
+class NewCodt7VisionCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        v = self.window.new_file()
+
+        v.assign_syntax('Packages/CoDT7 Formats/Vision.sublime-syntax')
+
+        prepend = 'vkTT "'
+        append  = '"\nvkTT ""\nvkTS ""\nvkTC ""\nvkTO ""\nvkRGB0 ""\nvkL0 ""\nvkM0 ""' \
+                  '\nvkRGB1 ""\nvkL1 ""\nvkM1 ""\nvkRGB2 ""\nvkL2 ""\nvkRGB3 ""\nvkL3 ""' \
+                  '\nvkM3 ""\nvkRGB4 ""\nvkL4 ""\nvkM4 ""\nvkRM ""'
+
+        insert_template(v, prepend, append)
+
+class NewCodt7SoundAliasCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        v = self.window.new_file()
+
+        v.assign_syntax('Packages/CoDT7 Formats/SoundAliasTable.sublime-syntax')
+
+        template = 'Name,Behavior,Storage,FileSpec,FileSpecSustain,FileSpecRelease,Template,Loadspec,Secondary,' \
+        'SustainAlias,ReleaseAlias,Bus,VolumeGroup,DuckGroup,Duck,ReverbSend,CenterSend,VolMin,VolMax,DistMin,' \
+        'DistMaxDry,DistMaxWet,DryMinCurve,DryMaxCurve,WetMinCurve,WetMaxCurve,LimitCount,LimitType,' \
+        'EntityLimitCount,EntityLimitType,PitchMin,PitchMax,PriorityMin,PriorityMax,PriorityThresholdMin,' \
+        'PriorityThresholdMax,AmplitudePriority,PanType,Pan,Futz,Looping,RandomizeType,Probability,StartDelay,' \
+        'EnvelopMin,EnvelopMax,EnvelopPercent,OcclusionLevel,IsBig,DistanceLpf,FluxType,FluxTime,Subtitle,Doppler,' \
+        'ContextType,ContextValue,ContextType1,ContextValue1,ContextType2,ContextValue2,ContextType3,ContextValue3,' \
+        'Timescale,IsMusic,IsCinematic,FadeIn,FadeOut,Pauseable,StopOnEntDeath,Compression,StopOnPlay,DopplerScale,' \
+        'FutzPatch,VoiceLimit,IgnoreMaxDist,NeverPlayTwice,ContinuousPan,FileSource,FileSourceSustain,FileSourceRelease,' \
+        'FileTarget,FileTargetSustain,FileTargetRelease,Platform,Language,OutputDevices,PlatformMask,WiiUMono,StopAlias,' \
+        'DistanceLpfMin,DistanceLpfMax,FacialAnimationName,RestartContextLoops,SilentInCPZ,ContextFailsafe,GPAD,GPADOnly,' \
+        'MuteVoice,MuteMusic,RowSourceFileName,RowSourceShortName,RowSourceLineNumber\n\n'
+
+        insert_template(v, template)
+
+        v.run_command('new_sound_alias_entry')
+
+
+class NewCodt7SoundReverbCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        v = self.window.new_file()
+
+        v.assign_syntax('Packages/CoDT7 Formats/SoundReverbTable.sublime-syntax')
+
+        template = 'Name,Loadspec,MasterReturn,EarlyInputLpf,EarlyFeedback,EarlySmear,EarlyBaseDelayMs,EarlyPreDelayMs,EarlyReturn,' \
+        'NearInputLpf,NearFeedback,NearReturn,NearLowDamp,NearHighDamp,NearDecayTime,NearSmear,NearPreDelayMs,FarInputLpf,' \
+        'FarFeedback,FarReturn,FarLowDamp,FarHighDamp,FarDecayTime,FarSmear,FarPreDelayMs\n\n'
+
+        insert_template(v, template)
+
+        v.run_command('new_sound_reverb_entry')
+
+class NewCodt7SoundT5AliasCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        v = self.window.new_file()
+
+        v.assign_syntax('Packages/CoDT7 Formats/SoundT5AliasTable.sublime-syntax')
+
+        template = 'name,file,template,loadspec,secondary,group,vol_min,vol_max,team_vol_mod,dist_min,dist_max,' \
+        'dist_reverb_max,volume_falloff_curve,reverb_falloff_curve,volume_min_falloff_curve,reverb_min_falloff_curve,' \
+        'limit_count,limit_type,entity_limit_count,entity_limit_type,pitch_min,pitch_max,team_pitch_mod,min_priority,' \
+        'max_priority,min_priority_threshold,max_priority_threshold,spatialized,type,loop,randomize_type,probability,' \
+        'start_delay,reverb_send,duck,pan,center_send,envelop_min,envelop_max,envelop_percentage,occlusion_level,' \
+        'occlusion_wet_dry,is_big,distance_lpf,move_type,move_time,real_delay,subtitle,mature,doppler,futz,context_type,' \
+        'context_value,compression,timescale,music,fade_in,fade_out,pc_format,pause,stop_on_death,bus,snapshot,voice_limit,' \
+        'file_xenon,file_size_xenon,file_ps3,file_size_ps3,file_pc,file_size_pc,file_wii,file_size_wii,source_csv,language\n\n'
+
+        insert_template(v, template)
+
+        v.run_command('new_sound_t5_alias_entry')
+
+class NewCodt7SoundT5ReverbCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        v = self.window.new_file()
+
+        v.assign_syntax('Packages/CoDT7 Formats/SoundT5ReverbTable.sublime-syntax')
+
+        template = 'name,room,roomHF,roomRolloffFactor,decayTime,decayHFRatio,reflections,reflectionsDelay,' \
+        'reverb,reverbDelay,diffusion,density,HFReference\n\n'
+
+        insert_template(v, template)
+
+        v.run_command('new_sound_t5_reverb_entry')
+
+class NewCodt7SoundAmbientCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        v = self.window.new_file()
+
+        v.assign_syntax('Packages/CoDT7 Formats/SoundAmbientTable.sublime-syntax')
+
+        template = 'Name,Loadspec,DefaultRoom,Reverb,NearVerb,FarVerb,ReverbDryLevel,ReverbWetLevel,' \
+        'EntityContextType0,EntityContextValue0,EntityContextType1,EntityContextValue1,EntityContextType2,' \
+        'EntityContextValue2,GlobalContextType,GlobalContextValue,Loop,Duck\n\n'
+
+        insert_template(v, template)
+
+        v.run_command('new_sound_ambient_entry')
