a69a674c1e0e69fec195e50487f8426cf24d69c6
Jake-NotTheMuss <jryan7852@gmail.com>
2022-01-06 21:37:42 -0800

    Clean up code

--- CoDT7 Formats/new_codt7_table_entry_templates.py
+++ CoDT7 Formats/new_codt7_table_entry_templates.py
@@ -1,23 +1,22 @@
 import sublime, sublime_plugin
 
-DEBUG_ON = False
+DEBUG = False
 
-def debug(*args):
-    if not DEBUG_ON:
+if DEBUG:
+    def debug(*args):
+        print(*args)
+else:
+    def debug(*args):
         return
-    print(*args)
 
 def replace_rows_with_template(view, edit, template):
     # list of cols from template string
     template_cols = template.split(',')
     # number of columns in template string
     n_template_cols = len(template_cols)
-    # position to put cursor after inserting template
-    final_cursor_pos = view.sel()[0].begin()
     # list of new selections to add afterward
     new_selections = []
     for region in view.sel():
-        debug('Inserting template into selection', region)
         # absolute position of cursor at this selection
         cursor_pos = region.begin()
         new_selections.append(cursor_pos)
@@ -27,10 +26,13 @@ def replace_rows_with_template(view, edit, template):
         original_line_size = line_region.size()
         # relative position of cursor in this line
         line_pos = cursor_pos - line_region.begin()
-        debug('Inserting template at position', line_pos, 'in line')
+        debug('Inserting template into selection',
+              region,
+              'at position',
+              line_pos,
+              'in line')
         # string of characters in the line
         str_line = view.substr(line_region)
-        debug('Line:', str_line)
         # list of column values in the line
         row_cols = str_line.split(',')
         # original number of columns before editing
@@ -42,6 +44,9 @@ def replace_rows_with_template(view, edit, template):
         # cursor might be past the number of expected
         # columns
         if n_leading_cols >= n_template_cols:
+            debug('Selection was beyond expected number of columns:',
+                  region,
+                  'Skipping selection')
             continue
         # new list of columns with the template inserted
         new_row_cols = row_cols[0:n_leading_cols] + template_cols[n_leading_cols:]
@@ -49,6 +54,10 @@ def replace_rows_with_template(view, edit, template):
         str_new_row = ','.join(new_row_cols)
         # number of characters in line after editing
         new_line_size = len(str_new_row)
+        debug('Number of characters in line changed from',
+              original_line_size,
+              'to',
+              new_line_size)
         # make the changes in the buffer
         view.replace(edit, line_region, str_new_row)
 
